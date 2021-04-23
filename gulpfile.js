a75ce6b80e438078255b7e48335b63f38c86c681
Jamie Tanner <jamietanner@gmail.com>
2021-04-23 10:55:57 -0400

    site ready to go

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,46 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+
+// css
 var cleanCss = require("gulp-clean-css");
+var postcss = require("gulp-postcss");
 var sourcemaps = require("gulp-sourcemaps");
+var concat = require("gulp-concat");
+
+// browser refresh
 var browserSync = require("browser-sync").create();
+
+// images
 var imagemin = require("gulp-imagemin");
+
+// github pages
 var ghpages = require("gh-pages");
 
-sass.compiler = require('node-sass');
 
-gulp.task("sass", function() {
-    // we want to run "sass css/app.scss app.css --watch"
-    return gulp.src("src/css/app.scss")
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(
-        cleanCss({
-            compatibility: 'ie8'
-        })
-    )
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest("dist"))
-    .pipe(browserSync.stream())
+gulp.task("css", function() {
+    return gulp.src([
+        "src/css/reset.css",
+        "src/css/typography.css",
+        "src/css/app.css"
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(
+            postcss([
+                require("autoprefixer"),
+                require("postcss-preset-env")({
+                    stage: 1,
+                    browsers: ["IE 11", "last 2 versions"]
+                })
+            ])
+        )
+        .pipe(concat("app.css"))
+        .pipe(
+            cleanCss({
+                compatibility: 'ie8'
+            })
+        )
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("dist"))
+        .pipe(browserSync.stream())
 })
 
 gulp.task("html", function() {
@@ -47,7 +67,7 @@ gulp.task("watch", function() {
     })
 
     gulp.watch("src/*.html", ["html"]).on("change", browserSync.reload)
-    gulp.watch("src/css/app.scss", ["sass"])
+    gulp.watch("src/css/*", ["css"])
     gulp.watch("src/fonts/*", ["fonts"])
     gulp.watch("src/img/*", ["images"])
 })
@@ -56,4 +76,4 @@ gulp.task("deploy", function() {
     ghpages.publish("dist")
 })
 
-gulp.task('default', ["html", "sass", "fonts", "images", "watch"]);
+gulp.task('default', ["html", "css", "fonts", "images", "watch"]);
